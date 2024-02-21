e08e1d88bf683561b6b1bc3faed64694ca059626
chenhan <1179991386@qq.com>
2024-02-21 15:26:39 +0800

    feat: Suspense

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,15 +9,10 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from '@/app/lib/data';
+import { fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -31,11 +26,11 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/* <RevenueChart revenue={revenue} /> */}
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart revenue={revenue} />
+          <RevenueChart />
         </Suspense>
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices latestInvoices={latestInvoices} />
+          <LatestInvoices />
         </Suspense>
       </div>
     </main>
