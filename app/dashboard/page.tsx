fdd8eb1366cc68246a5c82a61eef64e3cd1de623
chenhan <1179991386@qq.com>
2024-02-21 17:06:33 +0800

    feat: 增删改查

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,10 +9,8 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
-import { fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
